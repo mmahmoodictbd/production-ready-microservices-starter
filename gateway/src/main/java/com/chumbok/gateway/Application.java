cf91a76962087293ae819298a0be59302b42f120
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-05-30 00:32:24 +0200

    Added gateway service.

---
+++ gateway/src/main/java/com/chumbok/gateway/Application.java
@@ -0,0 +1,18 @@
+package com.chumbok.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableZuulProxy
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+
+}
