2ac93c7970df5e209f94734af3bd97190d4625eb
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-07-18 22:47:31 +0200

    Exception mapper conf.

---
+++ uaa/src/main/java/com/chumbok/uaa/conf/ExceptionMapperConfiguration.java
@@ -0,0 +1,18 @@
+package com.chumbok.uaa.conf;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.validation.ConstraintViolationProblemModule;
+
+@Configuration
+public class ExceptionMapperConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new ProblemModule())
+                .registerModule(new ConstraintViolationProblemModule());
+    }
+}
