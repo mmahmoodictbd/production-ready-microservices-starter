ff6fe1f590f0ef3f941d9b3e58bb622e6973cd2f
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-08-03 21:28:08 +0200

    Added eureka client config.

--- uaa/src/main/java/com/chumbok/uaa/conf/EurekaClientConfig.java
+++ uaa/src/main/java/com/chumbok/uaa/conf/EurekaClientConfig.java
@@ -1,10 +1,12 @@
 package com.chumbok.uaa.conf;
 
+
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDiscoveryClient
-public class EurekaClientConfiguration {
+public class EurekaClientConfig {
+
 }
 
