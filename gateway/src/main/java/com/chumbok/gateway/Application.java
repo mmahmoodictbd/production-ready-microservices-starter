42504c88e38329a8bdbf73273fc72d397954ba66
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-08-03 21:17:48 +0200

    Fix project after spring cloud version update.

--- gateway/src/main/java/com/chumbok/gateway/Application.java
+++ gateway/src/main/java/com/chumbok/gateway/Application.java
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
-	}
-
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
 
 }
