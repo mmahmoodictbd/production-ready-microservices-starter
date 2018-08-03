ac6b60f88778b321cddbbd0ef020c0b36bc2fb4e
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-08-03 21:27:05 +0200

    Added basic application.

--- uaa/src/main/java/com/chumbok/uaa/Application.java
+++ uaa/src/main/java/com/chumbok/uaa/Application.java
@@ -1,15 +1,20 @@
 package com.chumbok.uaa;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
