72a50595092f10c65d72c277295263c1feab944e
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-08-08 22:20:02 +0200

    Fetch eager roles from user.

--- uaa/src/main/java/com/chumbok/uaa/domain/model/User.java
+++ uaa/src/main/java/com/chumbok/uaa/domain/model/User.java
@@ -3,15 +3,14 @@ package com.chumbok.uaa.domain.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.MapKeyColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
@@ -97,7 +96,7 @@ public class User extends BaseEntity {
     /**
      * Roles
      */
-    @OneToMany(cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles = new HashSet<>();
 
     /**
