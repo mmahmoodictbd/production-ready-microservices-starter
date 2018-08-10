4e098994f033ceb854ff14e249c8459af7f3f84e
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-08-10 22:11:58 +0200

    Added refresh token.

---
+++ uaa/src/main/java/com/chumbok/uaa/security/token/RefreshToken.java
@@ -0,0 +1,60 @@
+package com.chumbok.uaa.security.token;
+
+import com.chumbok.uaa.exception.JwtExpiredTokenException;
+import com.chumbok.uaa.security.TokenScopes;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import org.springframework.security.authentication.BadCredentialsException;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public class RefreshToken implements JwtToken {
+
+    private Jws<Claims> claims;
+
+    private RefreshToken(Jws<Claims> claims) {
+        this.claims = claims;
+    }
+
+    /**
+     * Creates and validates Refresh token
+     *
+     * @param token
+     * @param signingKey
+     * @return
+     * @throws BadCredentialsException
+     * @throws JwtExpiredTokenException
+     */
+    public static Optional<RefreshToken> create(RawAccessJwtToken token, String signingKey) {
+
+        Jws<Claims> claims = token.parseClaims(signingKey);
+
+        List<String> scopes = claims.getBody().get("scopes", List.class);
+        if (scopes == null || scopes.isEmpty()
+                || !scopes.stream().filter(scope -> TokenScopes.REFRESH_TOKEN.authority().equals(scope)).findFirst()
+                .isPresent()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new RefreshToken(claims));
+    }
+
+    @Override
+    public String getToken() {
+        return null;
+    }
+
+    public Jws<Claims> getClaims() {
+        return claims;
+    }
+
+    public String getJti() {
+        return claims.getBody().getId();
+    }
+
+    public String getSubject() {
+        return claims.getBody().getSubject();
+    }
+}
