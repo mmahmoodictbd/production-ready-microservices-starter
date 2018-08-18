319978533606d504c76203a89486b5638d421bd8
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-08-19 00:00:41 +0200

    Remove unused security advice.

--- uaa/src/main/java/com/chumbok/uaa/conf/SecurityExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.chumbok.uaa.conf;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
-
-@ControllerAdvice
-public class SecurityExceptionHandler implements SecurityAdviceTrait {
-
-}
