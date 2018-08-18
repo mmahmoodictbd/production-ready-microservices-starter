2893f233fb5b76ad3df4ca22940d50f7dec4001c
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-08-19 00:33:11 +0200

    IT to insure zalando problem working correctly.

---
+++ uaa/src/integration-test/java/com/chumbok/uaa/conf/ExceptionHandlingAdviceIT.java
@@ -0,0 +1,42 @@
+package com.chumbok.uaa.conf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("it")
+public class ExceptionHandlingAdviceIT {
+
+    private static final String NON_EXISTENT_URL = "/not-exist";
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    public void shouldReturn404AndZalandoProblemJsonOnNotExistentUrl() throws Exception {
+
+        mockMvc.perform(get(NON_EXISTENT_URL))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.detail").value("No handler found for GET /not-exist"))
+                .andDo(print());
+    }
+
+}
