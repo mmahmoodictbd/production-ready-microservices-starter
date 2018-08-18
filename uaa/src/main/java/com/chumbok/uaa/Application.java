76947a257f6e38c0596a74475f50c1e142275380
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-08-18 23:53:32 +0200

    Fix Zalando Problem implementation.

--- uaa/src/main/java/com/chumbok/uaa/Application.java
+++ uaa/src/main/java/com/chumbok/uaa/Application.java
@@ -1,12 +1,15 @@
 package com.chumbok.uaa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
@@ -17,4 +20,5 @@ public class Application {
     void init() {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
+
 }
