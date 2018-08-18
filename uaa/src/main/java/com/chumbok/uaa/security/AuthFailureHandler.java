dc25379c47cf8f24e37e2b8af5158b01f842dd37
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-08-18 23:55:12 +0200

    Json response on Authentication exception.

--- uaa/src/main/java/com/chumbok/uaa/security/AuthFailureHandler.java
+++ uaa/src/main/java/com/chumbok/uaa/security/AuthFailureHandler.java
@@ -1,10 +1,13 @@
 package com.chumbok.uaa.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,17 +15,30 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * On authentication failure, this handler set proper http status.
- *
+ * On authentication failure, this handler set proper http status and write json response with message.
  */
 @Component
 public class AuthFailureHandler implements AuthenticationFailureHandler {
 
+    private ObjectMapper objectMapper;
+
+    public AuthFailureHandler(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-                                        AuthenticationException e) throws IOException, ServletException {
+                                        AuthenticationException ex) throws IOException, ServletException {
 
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        Problem problem = Problem.builder()
+                .withTitle("Authentication Problem")
+                .withStatus(Status.UNAUTHORIZED)
+                .withDetail(ex.getMessage()).build();
+        response.getWriter().write(objectMapper.writeValueAsString(problem));
+
     }
+
 }
