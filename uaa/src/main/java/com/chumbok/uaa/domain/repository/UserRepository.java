8532d703863de29ba86c1301e751d15926d7fec3
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-09-18 18:04:41 +0200

    Adjust UserRepository for multi tenancy.

--- uaa/src/main/java/com/chumbok/uaa/domain/repository/UserRepository.java
+++ uaa/src/main/java/com/chumbok/uaa/domain/repository/UserRepository.java
@@ -14,20 +14,22 @@ public interface UserRepository extends JpaRepository<User, Long> {
     /**
      * Checks if user exist.
      *
-     * @param domain
-     * @param email
+     * @param org
+     * @param tenant
+     * @param username
      * @return boolean true/false
      */
-    @Query("select count(user) > 0  from User user where user.domain = ?1 and user.email = ?2")
-    boolean isExist(String domain, String email);
+    @Query("select count(user) > 0  from User user where user.org = ?1 and user.tenant = ?2 and user.username = ?3")
+    boolean isExist(String org, String tenant, String username);
 
     /**
      * Retrieve User based on domain and email.
      *
-     * @param domain
-     * @param email
+     * @param org
+     * @param tenant
+     * @param username
      * @return User entity
      */
-    @Query("select user  from User user where user.domain = ?1 and user.email = ?2")
-    User findByDomainAndEmail(String domain, String email);
+    @Query("select user  from User user where user.org = ?1 and user.tenant = ?2 and user.username = ?3")
+    User find(String org, String tenant, String username);
 }
