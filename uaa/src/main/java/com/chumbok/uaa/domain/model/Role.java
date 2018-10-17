3d56481c6fc0850e1009456095603ce8915f6423
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-10-17 08:46:48 +0200

    Cleanup.

--- uaa/src/main/java/com/chumbok/uaa/domain/model/Role.java
+++ uaa/src/main/java/com/chumbok/uaa/domain/model/Role.java
@@ -10,7 +10,6 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 /**
