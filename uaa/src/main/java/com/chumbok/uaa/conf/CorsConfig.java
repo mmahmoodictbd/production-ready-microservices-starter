399f0dd8cc2a7ca97174ef6bb8340122f348f65b
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-10-19 10:38:20 +0200

    Added CORS.

---
+++ uaa/src/main/java/com/chumbok/uaa/conf/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.chumbok.uaa.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    // TODO: Make a conditional bean for dev and prod profile. Make sure wildcard allowed origin is removed.
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
