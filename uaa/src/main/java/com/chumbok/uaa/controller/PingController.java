905608e237b2379462a8457289d92c357a926f7d
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-10-26 09:12:43 +0200

    Fix class name.

--- uaa/src/main/java/com/chumbok/uaa/controller/PingController.java
+++ uaa/src/main/java/com/chumbok/uaa/controller/PingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PIngController {
+public class PingController {
 
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.NO_CONTENT)
