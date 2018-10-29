5f6f92f2d577e3004cbafaa0de578e27176b51e9
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-10-29 22:15:51 +0100

    Added app beans.

--- uaa/src/main/java/com/chumbok/uaa/conf/AppConfig.java
+++ uaa/src/main/java/com/chumbok/uaa/conf/AppConfig.java
@@ -2,9 +2,11 @@ package com.chumbok.uaa.conf;
 
 
 import com.chumbok.security.util.EncryptionKeyUtil;
+import com.chumbok.security.util.SecurityUtil;
 import com.chumbok.testable.common.CookieUtil;
 import com.chumbok.testable.common.DateUtil;
 import com.chumbok.testable.common.UrlUtil;
+import com.chumbok.testable.common.UuidUtil;
 import com.chumbok.uaa.util.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,5 +43,15 @@ public class AppConfig {
     @Bean
     public JwtUtil jwtUtil() {
         return new JwtUtil();
+    }
+
+    @Bean
+    public SecurityUtil securityUtil() {
+        return new SecurityUtil();
+    }
+
+    @Bean
+    public UuidUtil uuidUtil() {
+        return new UuidUtil();
     }
 }
