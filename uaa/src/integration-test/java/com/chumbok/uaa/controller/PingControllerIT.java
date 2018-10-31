1aa24b797e671ed80761b06f8988ae47ce67ac6e
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-10-31 11:25:23 +0100

    Fixed test.

--- uaa/src/integration-test/java/com/chumbok/uaa/controller/PingControllerIT.java
+++ uaa/src/integration-test/java/com/chumbok/uaa/controller/PingControllerIT.java
@@ -4,9 +4,11 @@ import com.chumbok.uaa.IntegrationTestBase;
 import io.restassured.response.Response;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
 
+@ActiveProfiles("it")
 public class PingControllerIT extends IntegrationTestBase {
 
     @Test
