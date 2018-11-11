20ead0818260e8392d420c73bdea6ec7348954de
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-11-11 19:53:21 +0100

    Added app configs.

---
+++ contact-service/src/main/java/com/chumbok/contact/config/AppConfig.java
@@ -0,0 +1,29 @@
+package com.chumbok.contact.config;
+
+import com.chumbok.security.util.SecurityUtil;
+import com.chumbok.testable.common.DateUtil;
+import com.chumbok.testable.common.UuidUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Load beans necessary objects.
+ */
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public DateUtil dateUtil() {
+        return new DateUtil();
+    }
+
+    @Bean
+    public UuidUtil uuidUtil() {
+        return new UuidUtil();
+    }
+
+    @Bean
+    public SecurityUtil securityUtil() {
+        return new SecurityUtil();
+    }
+}
---
+++ contact-service/src/main/java/com/chumbok/contact/config/EurekaClientConfig.java
@@ -0,0 +1,12 @@
+package com.chumbok.contact.config;
+
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaClientConfig {
+
+}
+
