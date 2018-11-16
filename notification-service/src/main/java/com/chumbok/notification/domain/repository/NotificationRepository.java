813b7ffd39ed758c44123726d3dfe559d46e102f
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-11-17 00:45:55 +0100

    Notification jpa repository.

---
+++ notification-service/src/main/java/com/chumbok/notification/domain/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.chumbok.notification.domain.repository;
+
+import com.chumbok.notification.domain.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
