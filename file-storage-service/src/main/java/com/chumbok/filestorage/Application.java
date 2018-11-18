f781fd8080c9435fe888ba43c741dbb3c9fa3914
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-11-18 04:23:49 +0100

    Basic scratch of file storage service.

---
+++ file-storage-service/src/main/java/com/chumbok/filestorage/Application.java
@@ -0,0 +1,27 @@
+package com.chumbok.filestorage;
+
+import com.chumbok.multitenancy.annotation.EnableMultitenantJpaRepositories;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EnableMultitenantJpaRepositories
+@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+}
+
