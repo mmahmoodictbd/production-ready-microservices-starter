f9a0520019678fd3fdd6865cf1f64c5dea0d62f9
mossaddeque <mmahmood.ict.bd@gmail.com>
2018-11-23 23:31:04 +0100

    Public private file endpoints.

--- file-storage-service/src/main/java/com/chumbok/filestorage/controller/FileStorageController.java
+++ file-storage-service/src/main/java/com/chumbok/filestorage/controller/FileStorageController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,13 +21,12 @@ import java.io.IOException;
 @Slf4j
 @AllArgsConstructor
 @Controller
-@RequestMapping("/files")
 public class FileStorageController {
 
     private final StorageService storageService;
 
-    @GetMapping("/**")
-    public ResponseEntity<Resource> downloadFile(HttpServletRequest request) {
+    @GetMapping("/public/files/**")
+    public ResponseEntity<Resource> downloadPublicFile(HttpServletRequest request) {
 
         String url = request.getRequestURL().toString().split("/files/")[1];
         if (url == null || url.trim().length() == 0) {
@@ -55,7 +53,13 @@ public class FileStorageController {
                 .body(resource);
     }
 
-    @PostMapping
+    @GetMapping("/files/**")
+    public ResponseEntity<Resource> downloadSecureFile(HttpServletRequest request) {
+        // TODO: implement it.
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @PostMapping("/files")
     public ResponseEntity<IdentityResponse> uploadFile(@RequestParam("file") MultipartFile file) {
         return new ResponseEntity<>(storageService.store(file), HttpStatus.CREATED);
     }
