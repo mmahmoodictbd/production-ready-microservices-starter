673f5c24b6df9aee13afb4ae6e5c67632762f124
mossaddeque <mmahmood.ict.bd@gmail.com>
2019-03-22 08:44:15 +0100

    Context load IT for file storage service.

---
+++ file-storage-service/src/integration-test/java/com.chumbok.filestorage/ContextLoadIT.java
@@ -0,0 +1,18 @@
+package com.chumbok.filestorage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("it")
+public class ContextLoadIT {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
