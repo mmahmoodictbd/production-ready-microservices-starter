8ae63cc7e11b878adba917c45c780e547d6d7d67
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-10-20 22:54:09 +0200

    Added cors filter bean for gateway.

--- gateway/src/main/java/com/chumbok/gateway/Application.java
+++ gateway/src/main/java/com/chumbok/gateway/Application.java
@@ -3,8 +3,12 @@ package com.chumbok.gateway;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -15,4 +19,17 @@ public class Application {
         new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
     }
 
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new org.springframework.web.filter.CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 }
